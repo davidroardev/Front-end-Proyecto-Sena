09d7281d397bf9d7442eba12a39f9dca73b60494
davidroardev <david_roa_06@hotmail.com>
2024-11-25 21:28:05 -0500

    Consumo de api para crear usuarios añadida

--- Scripts-back/login.js
+++ Scripts-back/login.js
@@ -1,6 +1,8 @@
 window.onload = (event) =>{
     const loginForm = document.getElementById('loginForm');
-    const loginMessagge = document.getElementById('loginMessage');
+    const loginMessage = document.getElementById('loginMessage');
+
+    const registerForm = document.getElementById('registerForm')
 
     loginForm.addEventListener('submit', async function(event){
 
@@ -15,22 +17,56 @@ window.onload = (event) =>{
                 headers: {
                     'Content-Type': 'aplication/json'
                 },
-                body: JSON.stringify({"userName":Name, "password": password})
+                body: JSON.stringify({userName:Name, password: password})
             });
 
             const data = await response.json();
             console.log(data);
             if (response.ok){
-                loginMessagge.textContent = 'Login Exitoso';
-                loginMessagge.style.color = 'green';
+                loginMessage.textContent = 'Login Exitoso';
+                loginMessage.style.color = 'green';
             }else{
-                loginMessagge.textContent = data.messge||'Error en el login';
-                loginMessagge.style.color = 'red';
+                loginMessage.textContent = data.messge||'Error en el login';
+                loginMessage.style.color = 'red';
             }
         } catch (error) {
             console.error(error)
             loginMessagge.textContent = 'Error en el servidor';
             loginMessagge.style.color = 'red';
+        }
+    });
+
+    registerForm.addEventListener('submit', async function(event){
+
+        const NewUserName = document.getElementById('NewUserName').value;
+        const Newpassword = document.getElementById('Newpassword').value;
+        const apellido = document.getElementById('name').value
+        const numeroTelefono = document.getElementById('phoneNumber').value
+        const direccion = document.getElementById('address').value
+        const email = document.getElementById('email').value
+        
+        try {
+            const response = await fetch('http://localhost:3000/user/register',{
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'aplication/json'
+                },
+                body: JSON.stringify({userName: NewUserName, password: Newpassword, apellido: apellido,numeroTelefonico: numeroTelefono,direccion: direccion,correoElectronico: email})
+            });
+
+            const data = await response.json();
+            console.log(data);
+            if (response.ok){
+                loginMessage.textContent = 'Login Exitoso';
+                loginMessage.style.color = 'green';
+            }else{
+                loginMessage.textContent = data.messge||'Error en el login';
+                loginMessage.style.color = 'red';
+            }
+        } catch (error) {
+            console.error(error)
+            loginMessage.textContent = 'Error en el servidor';
+            loginMessage.style.color = 'red';
         }
     });
 };
